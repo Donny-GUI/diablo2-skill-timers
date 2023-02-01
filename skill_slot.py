09bb3f0af8f449fde37ae057b86ffd3f2d19d535
Donny-GUI <djguiles3@gmail.com>
2023-02-01 15:58:24 -0800

    modified:   skill_slot.py 	modified:   setup_menu.py 	modified:   skill.py 	modified:   timer.py 	modified:   util.py

--- skill_slot.py
+++ skill_slot.py
@@ -6,21 +6,34 @@ from settings import Debug
 
 
 class SkillSlot:
+    
+    """ 
+    Used by the timer class, 
+    to reperesent a frame of skill, 
+    its meter and the timer associated with it 
+    """
+    
     def __init__(self, root, skill_name: str, duration_seconds: int, key_binding) -> None:
         self.name = skill_name
         self.root = root
         self.frame = Frame(root, bg='white')
         self.binding = key_binding
         
+        # SubClasses
+        
         self.skill = Skill(self.frame, skill_name)
         self.meter = Meter(self.frame)
         self.timer = Timer(duration_seconds)
 
+        # Pack  
+        
         self.skill.label.pack()
         self.meter.label.pack()
         
     
     def update(self):
+        """ Updates the element if it is started """
+        
         if self.timer.started == True:
             index = self.timer.frame_index
             self.meter.set_frame(index)
@@ -31,6 +44,8 @@ class SkillSlot:
                 self.skill.label.configure('fg','white')
     
     def reset(self):
+        """ resets the element if it is reset """
+        
         self.timer.running=False
         self.timer.thread_timer.join()
         self.timer.start()
